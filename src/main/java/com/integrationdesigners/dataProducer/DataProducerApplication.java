01de5f41d4c5b007cb2de98b840c95f06537d6ce
Zakaria <zakaria.elidrissi@student.unamur.be>
2022-05-05 10:06:15 +0200

    add : initial commit

---
+++ src/main/java/com/integrationdesigners/dataProducer/DataProducerApplication.java
@@ -0,0 +1,13 @@
+package com.integrationdesigners.dataProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProducerApplication.class, args);
+	}
+
+}
